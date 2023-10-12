a5021d77d9119f702351879896321ac962c34e82
trung.dv <trung.dv@geekup.vn>
2023-10-12 14:26:28 +0700

    Upgrade targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dv.lux.mail"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "dv.lux.mail"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
